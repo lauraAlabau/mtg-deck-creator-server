ace3e097858e48dc9ac7f4eb593cdac2c0644bb8
lauraAlabau <arlaura3@gmail.com>
2024-05-26 17:57:36 +0200

    add authentication and ownership to delete and update users

--- server/src/middlewares/index.ts
+++ server/src/middlewares/index.ts
@@ -1,4 +1,4 @@
-import { getUserBySessionToken } from "db/user";
+import { getUserBySessionToken } from "../db/user";
 import express from "express";
 import { get, merge } from "lodash";
 
@@ -22,6 +22,28 @@ export const isAuthenticated = async (
 
     merge(req, { identity: existingUser });
     return next();
+  } catch (error) {
+    console.log({ error });
+    return res.sendStatus(400);
+  }
+};
+
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const currenUserId = get(req, "identity._id") as string;
+
+    if (!currenUserId) {
+      return res.sendStatus(403);
+    }
+    if (currenUserId.toString() !== id) {
+      return res.sendStatus(403);
+    }
+    next();
   } catch (error) {
     console.log({ error });
     return res.sendStatus(400);
