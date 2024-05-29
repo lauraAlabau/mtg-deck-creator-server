f15ed3f88ac1d3de76b843113f00d507add6662d
lauraAlabau <arlaura3@gmail.com>
2024-05-29 20:05:09 +0200

    add footer ui component

--- client/src/app/_components/Navbar/index.tsx
+++ client/src/app/_components/Navbar/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import "./styles.css";
 
@@ -6,25 +7,19 @@ export const Navbar = () => {
   return (
     <nav className="nav-container">
       <div className="nav">
-        <Link href="/" className="nav-link">
+        <Link href="/">
           <img
             alt="logo"
             src="https://utfs.io/f/c41b25e1-91e1-49bd-bdf1-44f19f610f49-1evuz.webp"
           />
         </Link>
         <div className="nav-pages">
-          <Link href="/search" className="nav-link">
-            Search
-          </Link>
-          <Link href="/favorites" className="nav-link">
-            Favorites
-          </Link>
-          <Link href="/deck" className="nav-link">
-            Deck
-          </Link>
+          <Link href="/search">Search</Link>
+          <Link href="/favorites">Favorites</Link>
+          <Link href="/deck">Deck</Link>
         </div>
       </div>
-      <button className="nav-btn nav-link">
+      <button className="nav-btn ">
         Logout <FaArrowRightFromBracket />
       </button>
     </nav>
--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "./_components/Navbar";
+import { Footer, Navbar } from "./_components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className} suppressHydrationWarning={true}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
