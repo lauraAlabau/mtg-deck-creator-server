317dab38a6bc9be1d45f76ebb93302d7939815e4
lauraAlabau <arlaura3@gmail.com>
2024-06-06 18:33:06 +0200

    manage error in server side

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,15 +1,38 @@
 import express from 'express'
 import { UserModel } from '../models/User.js'
 import { validationResult } from 'express-validator'
+import bcrypt from 'bcrypt'
 
-
-const Register = (req, res) => {
+const Register = async (req, res) => {
   const errors = validationResult(req)
+
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() })
   }
 
-  return res.status(200).json("ok")
+  const { name, email, password } = req.body
+
+  try {
+    const userExist = await UserModel.findOne({ email })
+
+    if (userExist) {
+      return res.status(400).json({
+        errors: [{ msg: "User already exists" }],
+      })
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10)
+    const newUser = new UserModel({ name, email, password: hashPassword })
+    const result = await newUser.save()
+
+    result._doc.password = undefined
+
+    return res.status(201).json({ success: true, ...result._doc })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({ error: err.message })
+  }
+
 }
 
 export { Register }
