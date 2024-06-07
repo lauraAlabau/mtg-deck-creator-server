619834fd8da9e17a6d1eaa418bcaefa0acbb65b0
lauraAlabau <arlaura3@gmail.com>
2024-06-07 17:39:28 +0200

    add user menu in navbar

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,16 +1,21 @@
-
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { FaArrowRightFromBracket } from "react-icons/fa6";
+import { FaRegUserCircle } from "react-icons/fa";
+import { FiSettings } from "react-icons/fi";
 
 import { UserContext } from "../App";
 
 import "../assets/css/navbar.css";
 
 const Navbar = () => {
+  const { user } = useContext(UserContext);
+  const [open, setOpen] = useState(false);
 
-  const { user } = useContext(UserContext)
+  const handleOpen = () => {
+    setOpen(!open);
+  };
 
   return (
     <nav className="navbar">
@@ -22,20 +27,42 @@ const Navbar = () => {
           />
         </Link>
 
-        {user && <div className="navbar-pages">
-          <Link to="/search" className="navbar-link">Search</Link>
-          <Link to="/favorites" className="navbar-link">Favorites</Link>
-          <Link to="/deck" className="navbar-link">Deck</Link>
-        </div>}
-
+        {user && (
+          <div className="navbar-pages">
+            <Link to="/search" className="navbar-link">
+              Search
+            </Link>
+            <Link to="/favorites" className="navbar-link">
+              Favorites
+            </Link>
+            <Link to="/deck" className="navbar-link">
+              Deck
+            </Link>
+          </div>
+        )}
       </div>
 
-      {user && <button className="navbar-right">
-        Logout <FaArrowRightFromBracket />
-      </button>}
-
+      {user && (
+        <div className="navbar-right dropdown">
+          <button className="navbar-btn" onClick={handleOpen}>
+            <FaRegUserCircle />
+          </button>
+          {open ? (
+            <>
+              <div className="menu">
+                <div className="menu-item navbar-link">
+                  <FiSettings /><Link to="/profile">Settings</Link>
+                </div>
+                <div className="menu-item navbar-link">
+                  <FaArrowRightFromBracket /> <p>Logout </p>
+                </div>
+              </div>
+            </>
+          ) : null}
+        </div>
+      )}
     </nav>
   );
 };
 
-export default Navbar
+export default Navbar;
