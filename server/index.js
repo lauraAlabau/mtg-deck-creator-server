469ffe6c714b1997a1e502eb60876819e6ef7e90
lauraAlabau <arlaura3@gmail.com>
2024-06-09 13:54:26 +0200

    cors changes

--- server/index.js
+++ server/index.js
@@ -1,19 +1,25 @@
 import express from "express";
-import dotenv from 'dotenv'
-import cors from 'cors'
+import dotenv from "dotenv";
+import cors from "cors";
 
-import { Router } from './routes/routes.js'
+import { Router } from "./routes/routes.js";
 
-import './config/db.js'
+import "./config/db.js";
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["https://mtg-deck-creator-client.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
-dotenv.config({ path: "./config/.env" })
+dotenv.config({ path: "./config/.env" });
 
-app.use('/contactmsyt', Router)
+app.use("/contactmsyt", Router);
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server listening on port http://localhost:${process.env.PORT}`)
-})
+  console.log(`Server listening on port http://localhost:${process.env.PORT}`);
+});
