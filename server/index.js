58dd77a32a51f1424fe757d638c7403da490f819
lauraAlabau <arlaura3@gmail.com>
2024-06-09 14:27:05 +0200

    cors original

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["*"],
+    origin: ["https://mtg-deck-creator-client.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
